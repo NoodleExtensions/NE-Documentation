f1924cd65157f805656af5e462ba193253bc2e9e
cal117 <hendersoncal117@gmail.com>
2022-03-26 15:11:09 +0000

    Setup

--- site.config.js
+++ site.config.js
@@ -1,7 +1,12 @@
+const JSONC = require("jsonc-simple-parser");
+const fs = require('fs')
 module.exports = {
-  build: {
-    srcPath: './src',
-    outputPath: './public',
-    cleanURLs: true
-  }
+    build: {
+        srcPath: './src',
+        outputPath: './public',
+        cleanURLs: true
+    },
+    site: {
+        sidebar: JSONC.parse(fs.readFileSync("./src/pages/sidebar.jsonc", "utf-8"))
+    }
 };
